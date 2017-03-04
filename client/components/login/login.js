c39d85e78658db7cd297d65ee3f1bcf374a0fa66
probattle <probattle@iba.edu.pk>
2017-03-04 15:59:44 +0500

    Login 1

--- client/components/login/login.js
+++ client/components/login/login.js
@@ -11,7 +11,7 @@
 
     function LoginController($state, UserService) {
         var vm = this;
-        console.log("LOGIN")
+        console.log("LOGIN");
         this.email = "";
         this.password = "";
 
@@ -24,12 +24,17 @@
 
 
         this.loginUser = function () {
+            console.log("LOGIN FUNC");
+            console.log(vm.email);
+            console.log(vm.password);
 
-            firebase.auth().createUserWithEmailAndPassword(vm.email, vm.password).catch(function(error) {
+            firebase.auth().signInWithEmailAndPassword(vm.email, vm.password).catch(function(error) {
                 // Handle Errors here.
                 var errorCode = error.code;
                 var errorMessage = error.message;
                 // ...
+            }).then(function (data,error) {
+                console.log("asdasdasdasdasd");
             });
 
         }
